03416f41ba480c02f3cfbfae3fb7aca24b8cfaea
Asif Mahmud <asifofficial8130@gmail.com>
2024-01-12 01:39:14 +0600

    calculatwinner

--- src/Board.jsx
+++ src/Board.jsx
@@ -17,6 +17,26 @@ const Square = ({ value, onSquareClick }) => {
   );
 };
 
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
@@ -28,7 +48,7 @@ const Board = () => {
     }
     const nextSquares = squares.slice();
     // nextSquares[i] = "x";
-    xIsNext ? (nextSquares[i] = "x") : (nextSquares[i] = "O");
+    xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
 
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
