4e819011acadaf8969ea9e99fe51d17f716792b2
Asif Mahmud <asifofficial8130@gmail.com>
2024-01-11 12:38:35 +0600

    Game developed

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-const Square = () => {
-  const [value, setValue] = useState(null);
+const Square = ({ value }) => {
+  // const [value, setValue] = useState(null);
 
   const handleClick = () => {
-    setValue("X");
+    //  setValue("X");
   };
   return (
     <button
@@ -17,24 +17,25 @@ const Square = () => {
 };
 
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
 
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
 
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
